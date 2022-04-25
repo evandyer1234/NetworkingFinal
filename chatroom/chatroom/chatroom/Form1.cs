e869feca7272a37332d0f5c5f79d774fec0bb3bc
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2022-04-25 19:13:05 -0400

    update

---
+++ chatroom/chatroom/chatroom/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chatroom
+{
+    public partial class Form1 : Form
+    {        
+        public int us;
+        public int con;
+        public Form2 form2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            portui.Value = 8080;
+        }
+
+        private void enterchat(object sender, EventArgs e)
+        {
+            if (TB.Text != "")
+            {
+                Form2 f2 = new Form2();
+                f2.un = TB.Text;
+                f2.user = us;
+                f2.con = con;
+                f2.form1 = this;
+                form2 = f2;
+                f2.Show();
+            }
+        }
+        public void CloseForm()
+        {
+            form2.Close();
+        }
+        public void buttonreset(int i)
+        {
+            radioButton5.Enabled = true;
+            radioButton8.Enabled = true;
+            radioButton6.Enabled = true;
+            radioButton7.Enabled = true;
+
+            if (i == 1)
+            {
+                radioButton5.Enabled = false;
+            }
+            else if (i == 2)
+            {
+                radioButton6.Enabled = false;
+            }
+            else if (i == 3)
+            {
+                radioButton7.Enabled = false;
+            }
+            else if (i == 4)
+            {
+                radioButton8.Enabled = false;
+            }
+        }
+
+        private void rbcheck1(object sender, EventArgs e)
+        {
+            us = 1;
+            buttonreset(1);
+            
+        }
+
+        private void rbcheck2(object sender, EventArgs e)
+        {
+            us = 2;
+            buttonreset(2);
+        }
+
+        private void rbcheck3(object sender, EventArgs e)
+        {
+            us = 3;
+            buttonreset(3);
+        }
+
+        private void rbcheck4(object sender, EventArgs e)
+        {
+            us = 4;
+            buttonreset(4);
+        }
+
+        private void cn1(object sender, EventArgs e)
+        {
+            con = 1;
+        }
+
+        private void cn2(object sender, EventArgs e)
+        {
+            con = 2;
+        }
+
+        private void cn3(object sender, EventArgs e)
+        {
+            con = 3;
+        }
+
+        private void cn4(object sender, EventArgs e)
+        {
+            con = 4;
+        }
+
+       
+    }
+}
---
+++ chatroom/chatroom/chatroom/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace chatroom
+{
+
+    public partial class Form2 : Form
+    {
+        public int user;
+        public string un;
+        public int con;
+       
+        UdpClient _server = null;
+        IPEndPoint _client = null;
+
+        public Form2()
+        {
+            InitializeComponent();
+            _server = new UdpClient("127.0.0.1", 16000);
+            
+            _client = new IPEndPoint(IPAddress.Any, 0);
+        }
+
+        private void EnterPressed(object sender, EventArgs e)
+        {   
+            if (tb.Text != "")
+            {
+                AddMessage(user, tb.Text);
+                tb.Text = "";
+            }
+        }
+
+        public void AddMessage(int t, string s)
+        {
+            if (t == con || t == user)
+            {
+                //_server.Send(Encoding.ASCII.GetBytes(s), s.Length);
+                //byte[] data = _server.Receive(ref _client);
+                //string msg = Encoding.ASCII.GetString(data, 0, data.Length);
+
+                string p = un + ": " + tb.Text;
+                LB.Items.Add(p);
+            }
+        }
+
+        private void Leavebtn(object sender, EventArgs e)
+        {          
+            _server.Close();
+            this.Close();   
+        }
+    }
+}
